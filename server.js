6878fff2d1b743b4bce71d73fa8fd957a631d324
Guillermo <ggjnez92@gmail.com>
2022-01-11 10:22:29 +0100

    Only deploy server if file change

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const http = require('http');
 const port = +(process.env.PORT || 4538);
 
 const requestListener = function (req, res) {
+  console.log('login:', new Date());
   res.writeHead(200);
   res.end('authenticated');
 }
