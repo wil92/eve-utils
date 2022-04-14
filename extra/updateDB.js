7602700d341003e624e44ac3d9b70060468492a6
Guillermo Gonzalez <guillermo.gonzalez.ext@syrocon.de>
2022-04-14 23:31:33 +0200

    Add system class synchronization

--- extra/updateDB.js
+++ extra/updateDB.js
@@ -55,6 +55,28 @@ async function syncAction(database, ccpDatabase, tableLocal, paramsLocal, tableC
   });
 }
 
+async function syncWormholesAction(database, ccpDatabase) {
+  const sql = `UPDATE system SET system_class=? WHERE id=?;`;
+  const sql2 = `SELECT ss.solarSystemID, wc.wormholeClassID FROM mapSolarSystems ss INNER JOIN mapLocationWormholeClasses wc ON ss.regionID = wc.locationID;`;
+  await queryEACHCall(database, ccpDatabase, sql2, (row, next) => {
+    database.run(sql, [
+      +row['wormholeClassID'] <= 6 ? 'C' + row['wormholeClassID'] : +row['wormholeClassID'] === 7 ? 'HS' : +row['wormholeClassID'] === 9 ? 'NS' : 'C' + row['wormholeClassID'],
+      row['solarSystemID']
+    ], next);
+  });
+}
+
+async function syncLowSecAction(database, ccpDatabase) {
+  const sql = `UPDATE system SET system_class=? WHERE id=?;`;
+  const sql2 = `SELECT ss.solarSystemID, wc.wormholeClassID FROM mapSolarSystems ss INNER JOIN mapLocationWormholeClasses wc ON ss.solarSystemID = wc.locationID;`;
+  await queryEACHCall(database, ccpDatabase, sql2, (row, next) => {
+    database.run(sql, [
+      +row['wormholeClassID'] === 8 ? 'LS' : row['wormholeClassID'] + '',
+      row['solarSystemID']
+    ], next);
+  });
+}
+
 async function initDatabase(ccpDB, version) {
   const dbPath = path.join(__dirname, `${version}.data.db`);
 
@@ -110,6 +132,12 @@ async function initDatabase(ccpDB, version) {
     'station', ['id', 'name', 'office_rental_cost', 'reprocessing_efficiency', 'reprocessing_stations_take', 'system_id', 'type_id'],
     'staStations', ['stationID', 'stationName', 'officeRentalCost', 'reprocessingEfficiency', 'reprocessingStationsTake', 'solarSystemID', 'stationTypeID']);
 
+  // SYNC UNIVERSE SYSTEM'S CLASSES
+  isVerbose && console.log('Start wormhole, HS and NS classes synchronization');
+  await syncWormholesAction(database, ccpDatabase);
+  isVerbose && console.log('Start LS class synchronization');
+  await syncLowSecAction(database, ccpDatabase);
+
   // CLOSE DATABASES CONNECTION
   await new Promise((resolve, reject) => database.close((err) => err ? reject(err) : resolve()));
   await new Promise((resolve, reject) => ccpDatabase.close((err) => err ? reject(err) : resolve()));
@@ -126,4 +154,5 @@ shell
 
 shell
   .delimiter('db-script$')
-  .show();
+  .show()
+  .parse(process.argv);
