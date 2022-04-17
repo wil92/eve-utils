6316190f31ec5adb60bc5a57dca1eb39a8a0984e
Guillermo <ggjnez92@gmail.com>
2022-04-17 14:49:30 +0200

    Add cors to the server

--- server.js
+++ server.js
@@ -5,6 +5,14 @@ const path = require('path');
 const port = +(process.env.PORT || 4538);
 const downloadFolder = process.env.DOWNLOAD_FOLDER || './download';
 
+// CORS
+function injectCORS(res) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Request-Method', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET');
+  res.setHeader('Access-Control-Allow-Headers', '*');
+}
+
 // login code redirection code
 function loginController(req, res) {
   console.log('new authentication at:', new Date());
@@ -44,6 +52,8 @@ function downloadDBController(req, res) {
 
 
 const requestListener = function (req, res) {
+  injectCORS(res);
+
   if (req.url.startsWith('/download/db')) {
     downloadDBController(req, res);
     return;
